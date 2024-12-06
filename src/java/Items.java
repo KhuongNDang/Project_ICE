f4d43df83a303cb158ff2451fa2c37d165a05c7f
jasmintawfik <jasmintawfik7@gmail.com>
2024-12-06 12:07:58 +0100

    Items

---
+++ src/java/Items.java
@@ -0,0 +1,28 @@
+import java.util.ArrayList;
+
+public class Items {
+
+    private String name;
+    private int addAttack;
+    private int addDefend;
+    private int addHealth;
+    private int currency;
+    private ArrayList<Items> items;
+
+
+    public void getItem() {
+
+    }
+
+    public void sellItem() {
+
+    }
+
+    public void equipItem() {
+
+    }
+
+    public void buyItem() {
+
+    }
+}
