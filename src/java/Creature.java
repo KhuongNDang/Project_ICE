942d41eee0c3c40c12efe516f4da9d209b55a480
Zuhami <Aymanchaykh6@gmail.com>
2024-12-09 10:17:02 +0100

    alle metoder der updateret. som jeg har arbejdet med Ayman

--- src/java/Creature.java
+++ src/java/Creature.java
@@ -1,2 +1,26 @@
 public class Creature {
-}
+  private String name;
+   private int attack;
+   private int defense;
+   private int health;
+   private int currency;
+   private <List> items;
+
+
+
+        public createCreature() {
+
+        }
+
+    public getCreature() {
+              return creature;
+            }
+
+     public void setCreature () {
+             this.creature = creature;
+            }
+
+
+
+   }
+
---
+++ src/java/GameDialogue.java
@@ -0,0 +1,10 @@
+public class GameDialogue {
+
+       public void storyPart1(){
+        }
+        public void storyPart2(){
+        }
+        public void storyPart3(){
+        }
+
+}
