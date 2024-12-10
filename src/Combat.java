ae3732e978d1e441795c4b16c68ff78a39f875bf
Zuhami <Aymanchaykh6@gmail.com>
2024-12-10 12:45:47 +0100

    input metode

--- src/Combat.java
+++ src/Combat.java
@@ -1,57 +1,82 @@
+import java.util.ArrayList;
 import java.util.Random;
 
 public class Combat {
     private int attack;
     private int defense;
     private int health;
-
+    TextUI ui = new TextUI();
 
     public void fight(Player player, Creature creature) {
 
         for (int c = 1; player.getHealth() > 0 && creature.getHealth() > 0; c++) {
-            System.out.println(player.getName() + " and " + creature.getName() + " will now fight");
+            ui.Msg(player.getName() + " and " + creature.getName() + " will now fight");
+            ArrayList<String> options = new ArrayList<>();
+            options.add("proceed to Fight");
+            options.add("consume Item");
+            options.add("Flee");
 
+            int choice = ui.promptNumericChoice(options, "what`s your next Move?");
 
-            int playerAttack = player.getAttack();
-            int creatureAttack = creature.getAttack();
+            switch (choice) {
 
-            int playerDefense = player.getDefense();
-            int creatureDefense = creature.getDefense();
+                case 1:
+                    proceedTofight(player, creature);
+                    break;
+                case 2:
+                   // consume(player);
+                    break;
+                case 3:
+                   // flee(player, creature);
+                    return;
+            }
 
-            System.out.println(player.getName() + " attacks with " + playerAttack);
-            System.out.println(creature.getName() + " attacks with " + creatureAttack);
-
-
-            player.health -= creatureAttack - playerDefense;
-            creature.health -= playerAttack - creatureDefense;
-
-
-            System.out.println(creature.getHealth());
-            System.out.println(player.getHealth());
-
-
-            System.out.println();
-            System.out.println();
-
-
-
-
-
+        }
+        if(player.getHealth() <= 0) {
+            ui.Msg(player.getName() + " has been defeated.");
+        } else if (creature.getHealth() <= 0) {
+            ui.Msg(creature.getName() + " has been defeated.");
         }
     }
+
+
+    public void proceedTofight(Player player, Creature creature) {
+        int playerAttack = player.getAttack();
+        int creatureAttack = creature.getAttack();
+
+        int playerDefense = player.getDefense();
+        int creatureDefense = creature.getDefense();
+
+        System.out.println(player.getName() + " attacks with " + playerAttack);
+        System.out.println(creature.getName() + " attacks with " + creatureAttack);
+
+
+        player.health -= creatureAttack - playerDefense;
+        creature.health -= playerAttack - creatureDefense;
+
+
+        System.out.println(creature.getHealth());
+        System.out.println(player.getHealth());
+
+
+        System.out.println();
+        System.out.println();
+
+
+    }
 }
-
-
 /*
-        public void heavyAttack(){}
+    public void heavyAttack() {
+    }
 
-        public void lightAttack(){
-        }
+    public void lightAttack() {
+    }
 
-        public void consume(){}
+    public void consume(Player player) {
+    }
 
-        public void flee(){}
-
-
+    public void flee() {
+    }
 */
+
 
