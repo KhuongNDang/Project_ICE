6364e589b91071a12205be5469f4fc781397bfdb
jasmintawfik <jasmintawfik7@gmail.com>
2024-12-10 10:53:10 +0100

    Items

--- src/Player.java
+++ src/Player.java
@@ -6,13 +6,15 @@ public class Player {
     private int defense;
     public int health;
     private int xp;
+    private int currency;
 
-    public Player(String name, int attack, int defense, int health, int xp) {
+    public Player(String name, int attack, int defense, int health, int xp, int currency) {
         this.name = name;
         this.defense = defense;
         this.health = health;
         this.attack = attack;
         this.xp = xp;
+        this.currency = currency;
 
 
 //        public void equipItem () {
@@ -32,13 +34,15 @@ public class Player {
         return health;
     }
 
-    public int setHealth() {return health;}
+    public int setHealth() {
+        return health;}
 
     public int getXp() {
         return xp;
     }
 
-    public String getName() {return name;}
+    public String getName() {
+        return name;}
 
 
 }
