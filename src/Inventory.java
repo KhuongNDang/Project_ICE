3e930bccdc7fd96e8298a289d29a126ff430e308
jasmintawfik <jasmintawfik7@gmail.com>
2024-12-12 14:31:08 +0100

    Items

---
+++ src/Inventory.java
@@ -0,0 +1,37 @@
+
+import java.sql.Connection;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+
+
+
+    public class Inventory {
+        private List<Item> items;
+
+        //konstruktør
+        public Inventory(List<Item>items){
+            this.items = items;
+        }
+
+
+
+        public Inventory() {
+
+
+        }
+
+        public getItem() {
+
+        }
+
+        public removeItem() {
+
+        }
+
+
+        public addItem() {
+
+        }
+    }
+}
